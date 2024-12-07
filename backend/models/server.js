2958bda06ecec9fdfca25f966b00a9b9e2f9d6dc
unknown <herrera.fernando1209@gmail.com>
2024-12-07 02:55:58 -0300

    first commit

---
+++ backend/models/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 8080;
+
+    this.server = http.createServer(this.app);
+
+    this.io = socketio(this.server, {
+      /*Configuraciones */
+    });
+  }
+
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+  configSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    this.middlewares();
+
+    this.configSockets();
+
+    this.server.listen(this.port, () => {
+      console.log(`listening on ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
